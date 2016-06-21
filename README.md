06854af6d56bfd6183022264b4868596ff82d900
samzaza <samzaza@novellink.com>
2016-06-21 09:44:19 +0800

    first commit to push the repo to github

---
+++ README.md
@@ -0,0 +1,2 @@
+# bookstore
+Source code for my Tutorial "Test a Node RESTful API with Mocha and Chai"
---
+++ server.js
@@ -0,0 +1,47 @@
+
+let express = require('express');
+let app = express();
+let mongoose = require('mongoose');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+let port = 8080;
+let book = require('./app/routes/book');
+let config = require('config'); //we load the db location from the JSON files
+//db options
+let options = { 
+				server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } 
+              }; 
+
+//db connection      
+mongoose.connect(config.DBHost, options);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//don't show the log when it is test
+if(config.util.getEnv('NODE_ENV') !== 'test') {
+	//use morgan to log at command line
+	app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
+
+//parse application/json and look for raw text                                        
+app.use(bodyParser.json());                                     
+app.use(bodyParser.urlencoded({extended: true}));               
+app.use(bodyParser.text());                                    
+app.use(bodyParser.json({ type: 'application/json'}));  
+
+app.get("/", (req, res) => res.json({message: "Welcome to our Bookstore!"}));
+
+app.route("/book")
+	.get(book.getBooks)
+	.post(book.postBook);
+app.route("/book/:id")
+	.get(book.getBook)
+	.delete(book.deleteBook)
+	.put(book.updateBook);
+
+
+app.listen(port);
+console.log("Listening on port " + port);
+
+module.exports = app; // for testing
